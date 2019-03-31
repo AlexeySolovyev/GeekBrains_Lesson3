7aa5499c56dc56b2a22a1eaa87ae41c8bc87ddc4
Alexey Solovyev <solovevalex@gmail.com>
2019-03-31 13:38:11 +0300

    Initial state for ComputerTurn Smart Mode method.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -92,7 +92,9 @@ public class TicTacToe {
         } else {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
+
                     // проверяем клетки по направлениям
+
                 }
             }
         }
